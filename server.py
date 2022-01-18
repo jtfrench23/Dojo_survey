1cd00043246fe73e1dcd145bc1203854e1e82d28
jtfrench23 <92399356+jtfrench23@users.noreply.github.com>
2022-01-17 20:39:23 -0600

    put values into session

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, redirect, session, request
 app=Flask(__name__)
 app.secret_key='hereWeGoAgain'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/submit', methods=["POST"])
+def submission():
+    session['name']= request.form['name']
+    session['location']=request.form['location']
+    session['gender']=request.form['gender']
+    session['comments']=request.form['comments']
+    print(session['name'], session['location'], session['gender'], session['comments'])
+    return render_template('results.html')
+@app.route('/return', methods=["POST"])
+def back_home():
+    return redirect('/')
 
 
 
